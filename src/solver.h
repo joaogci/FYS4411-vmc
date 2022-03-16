e8c9b8781cd7d15df46f7cbe321d377e5b39b34d
João Inácio <jgci2000@hotmail.com>
2022-03-16 11:03:28 +0100

    formated prints and write_to_file working

--- src/solver.h
+++ src/solver.h
@@ -2,6 +2,10 @@
 #define SOLVER_H
 
 #include <iostream>
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <chrono>
 
 #include "rng.h"
 #include "systems/system.h"
@@ -14,13 +18,15 @@ private:
     System* system;
     MonteCarlo* mc_sampler;
 
-    long double *E;
-    long double *E2;
+    double alpha;
+    long double *E_sampled = NULL;
+    long double *E2_sampled = NULL;
+    long double run_time = 0;
 
     long mc_cycles;
     double equi_fraction;
     double measure_after;
-    double measure_cycles;
+    long measure_cycles;
 
     double eta = 1e-4;
     double tol = 1;
@@ -67,6 +73,9 @@ public:
         equi_fraction = equi_fraction_;
         measure_after = mc_cycles * equi_fraction;
         measure_cycles = mc_cycles * (1.0 - equi_fraction);
+
+        E_sampled = new long double[measure_cycles];
+        E2_sampled = new long double[measure_cycles];
     }
 
     ~Solver() {
@@ -80,6 +89,16 @@ public:
         equi_fraction = equi_fraction_;
         measure_after = mc_cycles * equi_fraction;
         measure_cycles = mc_cycles * (1.0 - equi_fraction);
+
+        if (E_sampled != NULL) {
+            delete[] E_sampled;
+        }
+        if (E2_sampled != NULL) {
+            delete[] E2_sampled;
+        }
+
+        E_sampled = new long double[measure_cycles];
+        E2_sampled = new long double[measure_cycles];
     }
 
     void solve(double alpha_) {
@@ -90,9 +109,14 @@ public:
         long double ratio;
 
         long double E_l, E = 0, E2 = 0;
+        long count = 0;
 
-        system->set_var_params(alpha_);
+        alpha = alpha_;
+        system->set_var_params(alpha);
 
+        printf("Starting VMC calculation with \nalpha: %.4f | mc_cycles: 2^%i | measure_after: 2^%i \n\n", alpha, (int) log2(mc_cycles), (int) log2(measure_after));
+
+        auto start = std::chrono::steady_clock::now();
         for (int t = 0; t < mc_cycles; t++) {
             idx_p = rng->rand() % N;
 
@@ -105,13 +129,19 @@ public:
                 E_l = system->local_energy();
                 E += E_l;
                 E2 += E_l * E_l;
+
+                E_sampled[count] = E_l;
+                E2_sampled[count] = E_l * E_l;
+                count++;
             }
         }
+        auto end = std::chrono::steady_clock::now();
+        run_time =  (double) std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() * pow(10.0, -9);
 
         E /= measure_cycles;
         E2 /= measure_cycles;
 
-        printf("Energy: %lf for alpha=%f \n", E, alpha_);
+        printf("alpha: %.4f | E: %4.6Lf | var: %4.6Lf | time: %fs \n\n", alpha_, E, E2 - E*E, run_time);
     }
 
     void set_optimization_params(double eta_, double tol_, double h_, long mc_cycles_, double equi_fraction_) {
@@ -130,20 +160,51 @@ public:
         long double derivative = tol + 1;
         long double Ep, Em;
 
+        long count = 0;
 
+        printf("Starting optimization with \neta: %.7f | tol: %.6f | h: %.6f | mc_cycles 2^%i \n\n", eta, tol, h, (int) log2(mc_cycles));
+
+        auto start = std::chrono::steady_clock::now();
         while (fabs(derivative) >= tol) {
             Ep = solve_optimizer(opt_alpha + h);
             Em = solve_optimizer(opt_alpha - h);
 
             derivative = (Ep - Em) / (2.0 * h);
             opt_alpha += - eta * derivative;
+
+            if (count % 10 == 0) {
+                printf("iter: %5li | alpha: %.4f | derivative: %6.5Lf \n", count, opt_alpha, derivative);
+            }
+            count++;
         }
+        auto end = std::chrono::steady_clock::now();
+        double time =  (double) std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() * pow(10.0, -9);
+
+        printf("opt_alpha: %.4f  | iter: %5li | time: %f\n\n", opt_alpha, count, time);
 
         return opt_alpha;
     }
 
-    void write_results();
-    
+    void write_results(std::string name, std::string path) {
+        std::string file_loc = path + name;
+        std::filesystem::create_directories(path);
+
+        std::ofstream file1(file_loc);
+        if (file1.is_open()) {
+            file1 << std::setprecision(10);
+            file1 << "alpha,runtime\n";
+            file1 << alpha << "," << run_time << "\n";
+            file1 << "energy,energy2\n";
+            for (int i = 0; i < measure_cycles; i++) {
+                file1 << E_sampled[i] << "," << E2_sampled[i] << "\n";
+            }
+            file1.close();
+        } else {
+            printf(" -- Error: can not open save file: \"%s\" -- \n", file_loc.c_str());
+        }
+
+        printf("Written results to file \"%s\" with success \n", file_loc.c_str());
+    }
 
 };
 
--- src/test.cpp
+++ src/test.cpp
@@ -6,15 +6,15 @@ int main() {
     System *ind_ho = new NonInteracting(100, 3, 1);
     MonteCarlo *met_sampler = new Metropolis(0.5);
     
-    Solver solver(ind_ho, met_sampler, (long) 1e5, 0.3, 2022);
+    Solver solver(ind_ho, met_sampler, 1e5, 0.3, 2022);
 
-    solver.set_optimization_params(1e-4, 1, 1e-3, 100000, 0.3);
+    solver.set_optimization_params(1e-4, 1, 1e-3, pow(2.0, 14), pow(2.0, -2));
     double opt_alpha = solver.optimize_var_params(0.1);
-    printf("opt_alpha %f \n", opt_alpha);
 
-    solver.set_solve_params(1e6, 0.15);
+    solver.set_solve_params(pow(2.0, 20), pow(2.0, -3));
     solver.solve(opt_alpha);
+
+    solver.write_results("file.csv", "./");
 
     return 0;
 }
-
