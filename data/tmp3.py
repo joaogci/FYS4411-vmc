69aa140a7a0ddf5ae343b1db6e206a244eb18777
João Inácio <jgci2000@hotmail.com>
2022-03-29 13:57:31 +0200

    #

--- data/tmp3.py
+++ data/tmp3.py
@@ -2,7 +2,7 @@ from process_data import process_data
 import os
 import sys
 
-directory = "./part_b_sl_comp/"
+directory = "./part_c_dt_comp/"
 folders = os.listdir(directory)
 try:
     folders.remove(".DS_Store")
@@ -10,8 +10,8 @@ except:
     ...
 folders.sort()
 
-sl = [0.001, 0.01, 0.1, 1.0, 10.0]
+dt = [0.001, 0.01, 0.1, 1.0, 10.0]
 
 for i, folder in enumerate(folders):
-    process_data(directory + folder + "/", "./data_sl" + str(sl[i]) + ".csv")
+    process_data(directory + folder + "/", "./data_dt" + str(dt[i]) + ".csv")
                 
