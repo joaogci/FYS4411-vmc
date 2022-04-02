c7a45acd1363b7eb68baadcd22a5296bf56fdb54
Joao <j.g.c.inacio@fys.uio.no>
2022-04-02 18:00:13 +0200

    parallelized the script

--- data/interaction.cpp
+++ data/interaction.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <cmath>
+#include <omp.h>
 
 #include "../src/vmc.h"
+
+#define NUM_THREADS 4
 
 int main(int argc, char **argv) {
     int N = atoi(argv[1]);
@@ -11,26 +14,37 @@ int main(int argc, char **argv) {
     long mc_cycles = pow(2, 24);
     double equi_fraction = pow(2.0, -4.0);
 
-    double eta = 1e-5;
-    double tol = 1e-4;
+    double eta = 5e-5;
+    double tol = 1e-6;
     double h = 1e-3;
-    long opt_cycles = pow(2, 20);
+    long opt_cycles = pow(2, 18);
     double opt_fraction = pow(2.0, -2.0);
 
     System *ind_ho = new Interacting(N, d, 1, a);
-    MonteCarlo *met = new Importance(0.1);
+    MonteCarlo *met = new Importance(0.001);
 
     Solver solver(ind_ho, met, mc_cycles, equi_fraction, (unsigned int) time(NULL));
     
     solver.set_optimization_params(eta, tol, h, opt_cycles, opt_fraction);
     double opt_alpha = solver.optimize_var_params(0.35);
 
-    solver.solve(opt_alpha);
-
-    solver.write_results("data_alpha" + std::to_string(opt_alpha) + "_N" + std::to_string(N) + ".csv", "./interaction/");
-
     delete ind_ho;
     delete met;
+
+    omp_set_num_threads(NUM_THREADS);
+    #pragma omp parallel 
+    {
+        System *ind_ho = new Interacting(N, d, 1, a);
+        MonteCarlo *met = new Importance(0.001);
+
+        Solver solver(ind_ho, met, mc_cycles / NUM_THREADS, equi_fraction, (unsigned int) time(NULL));
+        solver.solve(opt_alpha);
+        solver.write_results("data_interaction_thread" + std::to_string(omp_get_thread_num()) + "N" + std::string(N) + "_alpha" + std::to_string(opt_alpha) + ".csv", "./part_g_interaction/")
+    
+        delete ind_ho;
+        delete met;
+    }
+
 
     return 0;
 }
